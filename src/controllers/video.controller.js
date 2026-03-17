03a83b961ebc41ee0c0bb42edff118283ee1f3b6
Aman Kumar <tyagi13aman@gmail.com>
2026-03-17 13:29:03 +0530

    feat: enhance video management with update and delete functionalities; improve error handling

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -46,7 +46,7 @@ const publishAVideo = asyncHandler(async (req, res) => {
     videoFile: videoFile?.url || "",
     thumbnail: thumbnail?.url || "",
     duration: videoFile?.duration,
-    owner: req.user._id
+    owner: req.user._id,
   });
 
   console.log(video.owner);
@@ -59,29 +59,103 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: get video by id
-  console.log(videoId);
-  
-//   const video = await Video.findById(videoId)
+  const video = await Video.findById(videoId?.trim());
 
-//   if(!video) {
-//     throw new ApiError(400, "Video doesn't exist")
-//   }
+  if (!video) {
+    throw new ApiError(400, "Video does not exist");
+  }
 
-
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video fetched successfully"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: update video details like title, description, thumbnail
+  let thumbnailUrl;
+
+  if (req.file?.path) {
+    const uploaded = await uploadOnCloudinary(req.file.path);
+
+    if (!uploaded) {
+      throw new ApiError(400, "please reupload thumbnail");
+    }
+
+    thumbnailUrl = uploaded.url;
+  }
+
+  const { title, description } = req.body;
+
+  const updateFields = {};
+
+  if (title) updateFields.title = title;
+  if (description) updateFields.description = description;
+  if (thumbnailUrl) updateFields.thumbnail = thumbnailUrl;
+
+  const video = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: updateFields,
+    },
+    {
+      returnDocument: "after",
+      runValidators: true,
+    },
+  );
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Updated successfully!!"));
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: delete video
+  if (!videoId) {
+    throw new ApiError(400, "Video does not exist");
+  }
+
+  await Video.findByIdAndDelete(videoId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Video deleted successfully"));
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Invalid id");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "No such video exists");
+  }
+
+  const updatedVideo = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        isPublished: !video.isPublished,
+      },
+    },
+    {
+      returnDocument: "after",
+      runValidators: true,
+    }
+  );
+
+  return res.status(200).json(
+    new ApiResponse(200, updatedVideo, "Toggle updated successfully")
+  );
 });
 
 export {
